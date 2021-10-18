81eb3276806fe3add0ed65af13f3734b70da66ef
kavinsathyan <79074982+kavinsathyan@users.noreply.github.com>
2021-10-18 10:00:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+
+function setup(){
+  var canvas =  createCanvas(700,600);
+  canvas.parent('canvas');
+}
+
+
+function draw(){
+
+ background(0); 
+
+ fill("black");
+ stroke("black");
+ rect(680,0,20,700);
+
+ fill("black");
+ stroke("black");
+ rect(0,0,20,700);
+}
